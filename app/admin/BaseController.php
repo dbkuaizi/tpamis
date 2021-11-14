c44787c9b4b5a9dcb2bb3d838603e8ac2489df59
dbkuaizi <dbkuaizi@foxmail.com>
2021-11-14 23:34:14 +0800

    组件表改名 sys_com 增加通用删除接口 增加保存接口自动补全时间

--- app/admin/BaseController.php
+++ app/admin/BaseController.php
@@ -98,6 +98,17 @@ abstract class BaseController
      */
     public function success($msg,$data = [],$status = 0)
     {
+        // 如果传递的 msg 是数组则 赋值给 data
+        if (is_array($msg)) {
+            $data = $msg;
+            $msg = '';
+        }
         return ['status' => $status,'msg' => $msg,'data' => $data];
+    }
+
+    // 执行错误返回
+    public function error($msg,$status = 500)
+    {
+        return ['status' => $status,'msg' => $msg];
     }
 }
