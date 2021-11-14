08ae003bda3a3ffa6708bfd57b11c486651dc2fb
dbkuaizi <dbkuaizi@foxmail.com>
2021-11-14 23:35:31 +0800

    组件表改名 sys_com 新增通用删除接口 新增自动补全时间

--- app/admin/controller/Api.php
+++ app/admin/controller/Api.php
@@ -18,24 +18,29 @@ class Api extends BaseController
     // 通用数据获取接口
     public function get($code)
     {
-
-        $ret_data = ['status' => 0 ,'msg' => '数据获取成功','data' => []];
         $api_data = Db::table('sys_api')->where('v_Code',$code)->find();
-        switch ($api_data['v_Type']) {
-            case 'find': // 单条数据
-                $ret_data['data'] = $this->getFindData($api_data);
-                break;
-            case 'curd': // 多条数据
-                $ret_data['data'] = $this->getCurdData($api_data);
-                break;
-            case 'tree':
-                $ret_data['data'] = $this->getTreeData($api_data);
-                break;
-            case 'list': // 多条数据
-                $ret_data['data'] = $this->getListData($api_data);
-                break;
+
+        try {
+            switch ($api_data['v_Type']) {
+                case 'find': // 单条数据
+                    $ret_data = $this->getFindData($api_data);
+                    break;
+                case 'curd': // 多条数据
+                    $ret_data = $this->getCurdData($api_data);
+                    break;
+                case 'tree':
+                    $ret_data = $this->getTreeData($api_data);
+                    break;
+                case 'list': // 多条数据
+                    $ret_data = $this->getListData($api_data);
+                    break;
+            }
+        }catch (\Exception $e) {
+            $msg = 'Api Error ['. $code .']: '. $e->getMessage();
+            return $this->error($msg);
         }
-        return json($ret_data);
+
+        return $this->success('获取成功',$ret_data);
     }
 
     /**
@@ -44,13 +49,27 @@ class Api extends BaseController
     public function save(Request $request,$code,$id = '')
     {
         $ret_data = ['status' => 1 ,'msg' => '保存失败'];
-        $com_data = Db::table('sys_instances')->where('v_Code',$code)->field('v_Tables,v_PriField')->find();
+        $com_data = Db::table('sys_com')->where('v_Code',$code)->field('v_Tables,v_PriField')->find();
         $table_name = explode(',',$com_data['v_Tables'])[0];
         $data = $request->post();
+        // 获取操作表字段
+        $table_fields = array_column(DB::query("DESC ".$com_data['v_Tables']),'Field');
         if ($id){
             $pri_key = $com_data['v_PriField'] ?: 'i_Id';
+            // 是否需要补上修改时间
+            if (in_array('update_time',$table_fields)) {
+                $data['update_time'] =  date('Y-m-d H:i:s');
+            }
+
             $res = Db::table($table_name)->where([$pri_key => $id])->update($data);
         } else {
+
+            // 是否需要补上新增时间
+            if (in_array('create_time',$table_fields)) {
+                $data['create_time'] =  date('Y-m-d H:i:s');
+                $data['update_time'] =  date('Y-m-d H:i:s');
+            }
+
             $res = Db::table($table_name)->insert($data);
         }
         if ($res) {
@@ -58,6 +77,29 @@ class Api extends BaseController
         }
         return $ret_data;
 
+    }
+
+    // 通用删除接口
+    public function del($code,$id = '')
+    {
+        $com_data = Db::table('sys_com')->where('v_Code',$code)->field('v_Tables,v_PriField')->find();
+        // 如果组件不存在
+        if (!isset($com_data)) return $this->error('组件不存在');
+        if (empty($com_data['v_Tables'])) return $this->error('主键数据表不能为空');
+
+        // 判断是否存在软删除字段
+       $table_fields = array_column(DB::query("DESC ".$com_data['v_Tables']),'Field');
+       $pri_key = $com_data['v_PriField'] ?: 'i_Id';
+       // 如果存在软删除字段，就走软删除
+        $del_where[$pri_key] = $id;
+
+        if (in_array('delete_time',$table_fields)) {
+           $del_where['delete_time'] = NULL;
+           $ret = Db::table($com_data['v_Tables'])->where($del_where)->update(['delete_time' => date('Y-m-d H:i:s')]);
+       } else {
+          $ret = Db::table($com_data['v_Tables'])->where($del_where)->delete();
+       }
+        return $ret ? $this->success('删除成功') : $this->error('删除失败');
     }
 
     // 列表获取接口
