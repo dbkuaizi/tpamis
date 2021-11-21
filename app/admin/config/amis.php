4a7ae821b72215743738079a91ec7137474a5d77
dbkuaizi <dbkuaizi@foxmail.com>
2021-11-22 00:44:39 +0800

    登录页面完成

--- app/admin/config/amis.php
+++ app/admin/config/amis.php
@@ -4,10 +4,25 @@
 // +----------------------------------------------------------------------
 return [
     // AMIS 配置信息
-    'amis_name' => "ThinkAmis",
+    'amis_name'     =>  "ThinkAmis",
     // amis 使用模式、目前仅支持 app
-    'amis_mode' => 'app',
+    'amis_mode'     =>  'app',
     // amis 主题 antd、cxd、dark(夜间模式)、ang
-    'amis_theme' => 'ang',
-    "logo" => "/static/logo.png"
+    'amis_theme'    =>  'antd',
+    // login (推荐 50px * 50px)
+    "logo"  =>       "/static/logo.png",
+    "login" =>      [
+        // 登录窗口是否浮动 （默认高度 100%）
+        'float'     => true,
+        // 登录窗口位置（left、right）
+        'align'     => 'left',
+        // 登录窗口标题
+        'title'     =>  "Think Admin",
+        // 登录窗口版权
+        'footer'    => "Think Admin 后台快速开发框架 © 2021",
+        // 登录窗口背景图
+        'bg'        => 'url("http://cn.bing.com/th?id=OHR.ChurchillBears_EN-US8757524982_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp")',
+        // 登录表单宽度，仅pc有效
+        'width'     => '400px'
+    ],
 ];
