a0a4de28b874bf51e88a9220e5bd0f250fba743f
dbkuaizi <dbkuaizi@foxmail.com>
2021-12-25 15:24:55 +0800

    配置文件修改

--- config/amis.php
+++ config/amis.php
@@ -4,7 +4,7 @@
 // +----------------------------------------------------------------------
 return [
     // AMIS 配置信息
-    'amis_name'     =>  "ThinkAmis",
+    'amis_name'     =>  "tpamis",
     // amis 使用模式、目前仅支持 app
     'amis_mode'     =>  'app',
     // amis 主题 antd、cxd、dark(夜间模式)、ang
@@ -17,9 +17,9 @@ return [
         // 登录窗口位置（left、right）
         'align'     => 'left',
         // 登录窗口标题
-        'title'     =>  "Think Amis",
+        'title'     =>  "Tpamis Admin",
         // 登录窗口版权
-        'footer'    => "Think Amis 低代码开发框架 © 2021",
+        'footer'    => "Tpamis 低代码开发框架 © 2021",
         // 登录窗口背景图
         'bg'        => 'url("http://cn.bing.com/th?id=OHR.ChurchillBears_EN-US8757524982_1920x1080.jpg&rf=LaDigue_1920x1080.jpg&pid=hp")',
         // 登录表单宽度，仅pc有效
